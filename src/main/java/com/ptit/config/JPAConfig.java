6556fd73e74c231da0f2839aa4fe506e03bdcc26
= <=>
2021-12-24 10:35:48 +0700

    BaseEntity

--- src/main/java/com/ptit/config/JPAConfig.java
+++ src/main/java/com/ptit/config/JPAConfig.java
@@ -62,6 +62,7 @@ public class JPAConfig {
 		// properties.setProperty("hibernate.hbm2ddl.auto", "create-drop");
 		// properties.setProperty("hibernate.hbm2ddl.auto", "create");
 		properties.setProperty("hibernate.hbm2ddl.auto", "none");
+		// properties.setProperty("hibernate.hbm2ddl.auto", "update");
 		return properties;
 	}
 }
